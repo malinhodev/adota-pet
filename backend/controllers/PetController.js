e3886cf277bbb69741097d63137138808fa105d6
malinhodev <mcterasgo@gmail.com>
2023-02-25 10:20:30 -0300

    concluindo rota de pet adotado

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -268,4 +268,37 @@ module.exports = class PetController {
             message: `Agendado com sucesso! entre em contato com ${pet.user.name} pelo telefone ${pet.user.phone}.`
         })
     }
+
+    static async concludeAdoption(req,res){
+        const id = req.params.id
+        //check if ID is valid
+        if(!ObjectId.isValid(id)){
+            res.status(422).json({ mesage: 'ID inválido!'})
+            return
+        }
+        //check if pet exists
+        const pet = await Pet.findOne({ _id: id})
+        if(!pet){
+            res.status(404).json({
+                message: 'Pet não encontrado!'
+            })
+            return
+        }
+        //check if logged in user registered the pet
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+        if(pet.user._id.toString() !== user._id.toString()){
+            res.status(422).json({
+                message: 'Houve um problema, tente novamente mais tarde!'
+            })
+            return
+        }
+
+        pet.available = false
+
+        await Pet.findByIdAndUpdate(id,pet)
+        res.status(200).json({
+            message: 'Parabéns, Adoção concluída!'
+        })
+    }
 }
