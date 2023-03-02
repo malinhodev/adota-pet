7ca0070c21279a4cf942ad763959155e45b79a65
malinhodev <mcterasgo@gmail.com>
2023-03-02 13:18:38 -0300

    iniciando router react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
-import {BrowerRouuter as Router,Switch,Route} from 'react-router-dom'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+
+//pages
+import Login from './components/pages/Auth/Login'
+import Register from './components/pages/Auth/Register'
+import Home from './components/pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Adota-pet</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login/>} />
+             
+        <Route path='/register' element={<Register/>}/>
+             
+        <Route path='/' element={<Home/>} />
+              
+      </Routes>
+    </Router>
   );
 }
 
